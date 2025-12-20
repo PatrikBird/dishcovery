01b40a10bc31b23eb7db25a93760ce7a7a3b605a
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2025-12-20 13:36:17 +0100

    chore: remove ui draft again

--- ui/nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/ui"],
-  css: ["~/assets/css/main.css"],
-  ssr: false,
-});
